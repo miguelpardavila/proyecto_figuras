66558d707ac0989e569386ab21f6e7c5b217b3cf
Miguel Pardavila <miguel.pardavila@rai.usc.es>
2025-09-30 18:45:32 +0200

    Creadas las funciones del triángulo

---
+++ lib/triangulo.py
@@ -0,0 +1,10 @@
+get_identificador(id):
+	id="triangulo"
+	return id
+get_area(base,altura):
+	area=base*altura
+	return area
+get_perimetro(lado_a,lado_b,lado_c):
+	perimetro=lado_a+lado_b+lado_c
+	return perimetro
+
