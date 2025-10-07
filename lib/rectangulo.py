8263e21387feb63cd9e0e654c67fd55f2a363fc5
Miguel Pardavila <miguel.pardavila@rai.usc.es>
2025-10-07 16:00:54 +0200

    Agregadas funcionalidades para el rectángulo

---
+++ lib/rectangulo.py
@@ -0,0 +1,12 @@
+def get_area(base,altura):
+    area_rectangulo=base*altura
+    return area_rectangulo
+
+def get_identificador(palabra):
+	palabra="rectangulo"
+	return palabra
+
+def get_perimetro(lado1,lado2):
+	perimetro=(lado1*2)+(lado2*2)
+	return perimetro
+
